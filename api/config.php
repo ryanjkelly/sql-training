fddfadcdaa5e9d9c1c4c4d6894eaca53beef21a8
Ryan Kelly <ryan@kellybusinessservices.com>
2018-06-07 20:42:09 -0500

    added config

---
+++ api/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'ROOT_PATH', dirname( dirname( dirname( __FILE__ ) ) ) );
+
+define( 'ABS_PATH', dirname( dirname( __FILE__ ) ) );
+
+define( 'CREDENTIALS_PATH', ROOT_PATH . '/credentials' );
+
+define( 'API_PATH', ABS_PATH . '/api' );
---
+++ api/truncate-table/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once( '../config.php' );
+
+require_once( CREDENTIALS_PATH . '/sql.php' );
